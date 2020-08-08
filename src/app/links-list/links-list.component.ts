b93039462a6225cd8acca3613a1ea34f16853e90
Vivek S <vivek.s@atonarp.com>
2020-08-08 22:57:26 +0530

    some working examples

--- src/app/links-list/links-list.component.ts
+++ src/app/links-list/links-list.component.ts
@@ -16,4 +16,14 @@ export class LinksListComponent implements OnInit {
   {
     this.expandYT = !this.expandYT
   }
+
+  navigate(link)
+  {
+    if(link == "insta")
+      window.open("intent://www.instagram.com/vivekgowdaa_/#Intent;package=com.instagram.android;scheme=https;end?igshid=plxe937czeaq")
+    if(link == "fb")
+      window.open("intent://profile/100005215591352#Intent;package=com.facebook.katana;scheme=fb;end")
+    if("wp")
+      window.open("https://api.whatsapp.com/send?phone=917760448418&text=Hi")
+    }
 }
